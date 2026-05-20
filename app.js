673cfd8495d7838c2b4baa1fd38cc0ef356ea3ba
dmitro123ads <bober228dima@gmail.com>
2026-05-20 19:10:30 +0300

    2modal added

--- app.js
+++ app.js
@@ -12,3 +12,18 @@
         refs.modal.classList.toggle("is-hidden");
     }
 })();
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open2]"),
+        closeModalBtn: document.querySelector("[data-modal-close2]"),
+        modal: document.querySelector("[data-modal2]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
